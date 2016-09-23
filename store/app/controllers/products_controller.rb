a980499f71ed189fc432d0d1b805cf536fc42206
sofiamatulis <sosomatulis@hotmail.com>
2016-09-22 23:20:08 -0400

    created index show page for views

--- store/app/controllers/products_controller.rb
+++ store/app/controllers/products_controller.rb
@@ -1,7 +1,18 @@
 class ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:id])
+
+
   end
   def index
+    @products = Product.all
+
   end
-  
+
+  private
+
+  def product_params
+      params.require(:product).permit(:title, :price)
+    end
+
 end
