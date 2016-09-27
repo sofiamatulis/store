d595ad4c4a1b26041f6c0f4c7970c3265a95f6aa
sofiamatulis <sosomatulis@hotmail.com>
2016-09-27 10:50:04 -0400

    created items page

--- store/db/seeds.rb
+++ store/db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-Product.create(title: 'Robot', price:12)
-Product.create(title: 'Clock', price:13)
-Product.create(title: 'Watch', price:20)
-Product.create(title: 'Phone', price:5)
+
+Product.create!(title: 'Shopify 1', price:29, color:'blue',description:'This is a regular plan')
+Product.create!(title: 'Shopify 2', price:79,color:'green',description:'This is an advanced plan')
+Product.create!(title: 'Advanced Shopify 3', price:299,color:'yellow',description:'This is the most advanced plan')
